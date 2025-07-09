0a5aed0bf3b0242be7c6e4dbb9c25d7624429ff5
KaguyaCode <waterlilypetal@proton.me>
2025-07-09 13:00:59 +0100

    readding with meta files

---
+++ KaguyaTimeOfDay/KaguyaTimeOfDayUICore.cs
@@ -0,0 +1,104 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using VRC.SDKBase;
+using VRC.Udon;
+using KaguyaCode.UI;
+
+namespace KaguyaCode.UI
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class KaguyaTimeOfDayUICore : UdonSharpBehaviour
+    {
+        [Header("Put Buttons Here")]
+        [SerializeField] private KaguyaTimeOfDayUIButton morningButton;
+        [SerializeField] private KaguyaTimeOfDayUIButton dayButton;
+        [SerializeField] private KaguyaTimeOfDayUIButton sunsetButton;
+        [SerializeField] private KaguyaTimeOfDayUIButton nightButton;
+        
+        [Header("Put Object with Audiosource Here")]
+        [Tooltip("This is local - Plays whenever a button is selected or deselected")]
+        [SerializeField] private AudioSource selectionAudio;
+        
+        [UdonSynced] private int selectedIndex = -1;
+        private VRCPlayerApi localPlayer;
+
+        private void Start()
+        {
+            localPlayer = Networking.LocalPlayer;
+            UpdateVisuals();
+        }
+
+        public override void OnDeserialization()
+        {
+            UpdateVisuals();
+        }
+
+        private void UpdateVisuals()
+        {
+            if (morningButton != null) 
+                morningButton.SetSelected(selectedIndex == 0);
+            if (dayButton != null) 
+                dayButton.SetSelected(selectedIndex == 1);
+            if (sunsetButton != null) 
+                sunsetButton.SetSelected(selectedIndex == 2);
+            if (nightButton != null) 
+                nightButton.SetSelected(selectedIndex == 3);
+        }
+
+        public void ClearAllSelection()
+        {
+            selectedIndex = -1;
+            UpdateVisuals();
+
+            if (!Networking.IsOwner(gameObject))
+                Networking.SetOwner(localPlayer, gameObject);
+    
+             RequestSerialization();
+        }
+
+        public void SelectMorning()
+        {
+            SetActiveButtonTime(0);
+        }
+        
+        public void SelectDay()
+        {
+            SetActiveButtonTime(1);
+        }
+        
+        public void SelectSunset()
+        {
+            SetActiveButtonTime(2);
+        }
+        
+        public void SelectNight()
+        {
+            SetActiveButtonTime(3);
+        }
+
+        private void SetActiveButtonTime(int index)
+        {
+            if (selectionAudio != null) 
+                selectionAudio.Play();
+
+            if (selectedIndex == index)
+            {
+                selectedIndex = -1;
+                // Can put something else here if needed when deselecting
+            }
+            else
+            {
+                selectedIndex = index; // Sets new button
+            }
+
+            if (!Networking.IsOwner(gameObject))
+            {
+                Networking.SetOwner(localPlayer, gameObject);
+            }
+            
+            RequestSerialization();
+            UpdateVisuals();
+        }
+    }
+}
